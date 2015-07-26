5eee285b9abccb2ffe8c902c2ead2e11132db91e
Ken Ang <angkendrick@gmail.com>
2015-07-26 17:04:26 -0400

    part 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post' #necessary for the update action
+  patch 'posts/:id', to: 'posts#update'
   post 'posts', to: 'posts#create'
 
   # get 'posts/0', to: 'posts#post0' #STATIC ROUTING
