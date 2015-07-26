46d6d272da44ef583678ac021079c29f314ab468
Ken Ang <angkendrick@gmail.com>
2015-07-26 16:53:59 -0400

    part 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,31 @@
 class PostsController < ApplicationController
 
   def index
-
-      @posts = Post.all
-
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
 
-      @post = Post.find(params[:id])
-    
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
   end
 
   #   @posts = [ #old index file.. replaced with db
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts', to: 'posts#create'
 
   # get 'posts/0', to: 'posts#post0' #STATIC ROUTING
   # get 'posts/1', to: 'posts#post1'
